e693730fb0cdf251ee77bf891ede97cfc995744b
MattAlanWright <matt.alan.wright@gmail.com>
2022-06-30 11:53:52 -0400

    main: Improved 'help' results for individual commands

--- main.go
+++ main.go
@@ -19,23 +19,27 @@ func main() {
 		&cli.BoolFlag{
 			Name:        "debug",
 			Aliases:     []string{"d"},
-			Usage:       "Set to print debugging information",
+			Usage:       "Print debugging information",
 			Destination: &debugMode,
 		},
 	}
 
 	commands := []*cli.Command{
 		{
-			Name:   "refresh",
-			Usage:  "Refresh repository list",
-			Action: entryPoint,
-			Flags:  flags,
+			Name:        "refresh",
+			Usage:       "Refresh repository list",
+			Action:      entryPoint,
+			Flags:       flags,
+			UsageText:   "yeet refresh",
+			Description: "Generates a list of repos across which to perform the target rebase and saves the results to repolist.json. Uses the `repo list` command.",
 		},
 		{
-			Name:   "rebase",
-			Usage:  "Rebase target branch onto the tip of main on all repos",
-			Action: entryPoint,
-			Flags:  flags,
+			Name:        "rebase",
+			Usage:       "Rebase target branch onto the tip of main across all repos",
+			Action:      entryPoint,
+			Flags:       flags,
+			UsageText:   "yeet rebase <targetbranch>",
+			Description: "Rebases origin/<targetbranch> onto the tip of origin/main across all repos. All repositories that do not have the branch origin/<targetbranch> are updated to the tip of origin/main. repolist.json must exist.",
 		},
 	}
 
